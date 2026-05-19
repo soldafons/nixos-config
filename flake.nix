66e16ab286084f9ea50a5562db23380ba8e26ef4
soldafons <medoviysup889@gmail.com>
2026-05-19 22:43:49 +0300

    Updated: Switched to Lix

--- flake.nix
+++ flake.nix
@@ -16,18 +16,29 @@
       url = "github:nix-community/lanzaboote/v1.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # mangow
+    # mangowm
     mangowm = {
       url = "github:mangowm/mango";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # stylix
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # lix
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
   };
 
-  outputs = inputs @ { self, nixpkgs, flake-parts, home-manager, lanzaboote, mangowm, stylix, ... }:
+  outputs = inputs @ { self, nixpkgs, flake-parts, home-manager, lanzaboote, mangowm, stylix, lix-module, lix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       flake = {
         nixosConfigurations = {
@@ -41,6 +52,7 @@
               lanzaboote.nixosModules.lanzaboote
               mangowm.nixosModules.mango
               stylix.nixosModules.stylix
+              lix-module.nixosModules.default
             ];
           };
         };
